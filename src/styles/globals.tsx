eb53aac0412803ef39a68d52fb6683ec033f267f
Nathan Houle <nathan@nathanhoule.com>
2023-01-20 17:24:56 -0800

    fix: correctly scale fonts on mobile

--- src/styles/globals.tsx
+++ src/styles/globals.tsx
@@ -4,6 +4,10 @@ import { inter, lato } from "./fonts";
 import { hsl } from "./utils";
 import { Global, css } from "@emotion/react";
 
+/**
+ * Headers are modularly scaled at a 1.25 ratio on desktop, 1.15 on mobile.
+ * https://www.modularscale.com/?1=&em=&1.25=
+ */
 const typographyStyles = css({
   html: {
     color: `${hsl(softBlack)}`,
@@ -11,7 +15,7 @@ const typographyStyles = css({
     fontSize: "16px",
     letterSpacing: "0.15px",
 
-    [`& @media only screen and (max-width: ${breakpoints.sm.px})`]: {
+    [`@media only screen and (max-width: ${breakpoints.sm.px})`]: {
       fontSize: "15px",
     },
   },
@@ -24,31 +28,55 @@ const typographyStyles = css({
   h1: {
     fontSize: "3.815em",
     letterSpacing: "-1.5px",
+
+    [`@media only screen and (max-width: ${breakpoints.sm.px})`]: {
+      fontSize: "2.986em",
+    },
   },
 
   h2: {
     fontSize: "3.052em",
     letterSpacing: "-0.5px",
+
+    [`@media only screen and (max-width: ${breakpoints.sm.px})`]: {
+      fontSize: "2.488em",
+    },
   },
 
   h3: {
     fontSize: "2.441em",
     letterSpacing: "0px",
+
+    [`@media only screen and (max-width: ${breakpoints.sm.px})`]: {
+      fontSize: "2.074em",
+    },
   },
 
   h4: {
     fontSize: "1.953em",
     letterSpacing: "0.25px",
+
+    [`@media only screen and (max-width: ${breakpoints.sm.px})`]: {
+      fontSize: "1.728em",
+    },
   },
 
   h5: {
     fontSize: "1.563em",
     letterSpacing: "0px",
+
+    [`@media only screen and (max-width: ${breakpoints.sm.px})`]: {
+      fontSize: "1.44em",
+    },
   },
 
   h6: {
     fontSize: "1.25em",
     letterSpacing: "0.15px",
+
+    [`@media only screen and (max-width: ${breakpoints.sm.px})`]: {
+      fontSize: "1.2em",
+    },
   },
 });
 
