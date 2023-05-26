66965d71eb5d58cff4b8ee0b1f3999b4844282aa
Nathan Houle <nathan@nathanhoule.com>
2023-05-26 13:33:39 -0700

    feat: bump up start time for PCT trip

--- src/app/trips/2023/pacific-crest-trail/page.tsx
+++ src/app/trips/2023/pacific-crest-trail/page.tsx
@@ -21,7 +21,7 @@ const config = {
   photoAlbumId: "3fc46ea9-cf1e-4933-b160-d5e957b60d9c",
   tripEnd: "2023-10-01T17:00:00.000Z",
   tripId: "pacific-crest-trail",
-  tripStart: "2023-05-27T17:00:00.000Z",
+  tripStart: "2023-05-26T17:00:00.000Z",
 };
 
 export const metadata: Metadata = {
